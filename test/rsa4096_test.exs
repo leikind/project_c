300206b1d92039059891ddf784945ca25b7e9173
Yuri Leikind <yuri.leikind@profy.be>
2020-03-08 12:18:47 +0100

    Elixir 1.10

---
+++ test/rsa4096_test.exs
@@ -0,0 +1,13 @@
+defmodule Rsa4096Test do
+  use ExUnit.Case
+
+  alias ProjectC.Rsa4096
+
+  test "rsa_encypt_decrypt_with_ex_public_key" do
+    assert Rsa4096.rsa_encypt_decrypt_with_ex_public_key("this is love!!!") == "this is love!!!"
+  end
+
+  test "rsa_encypt_decrypt_pure_erlang_crypto" do
+    assert Rsa4096.rsa_encypt_decrypt_pure_erlang_crypto("is this love?") == "is this love?"
+  end
+end
